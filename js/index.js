4772388485605d1e3e53522e0d2267432138fe5a
uemurayoko <y.uemura11@gmail.com>
2021-08-25 21:16:29 +0900

    works3.ihtml追加、imgに2点追加、works3に対するCSSとindex.html内の記入追加 ハンバーガーメニューのリンクをクリック時にハンバーガーメニューを閉じるようJS追加 ハンバーガーメニューの右側にpaddingを追加

--- js/index.js
+++ js/index.js
@@ -2,11 +2,19 @@ function toggleNav() {
   var body = document.body;
   var hamburger = document.getElementById('js-hamburger');
   var blackBg = document.getElementById('js-black-bg');
+  var work = document.getElementById('hbg_work');
+  var about = document.getElementById('hbg_about');
 
   hamburger.addEventListener('click', function() {
     body.classList.toggle('nav-open');
   });
   blackBg.addEventListener('click', function() {
+    body.classList.remove('nav-open');
+  });
+  work.addEventListener('click', function() {
+    body.classList.remove('nav-open');
+  });
+  about.addEventListener('click', function() {
     body.classList.remove('nav-open');
   });
 }
